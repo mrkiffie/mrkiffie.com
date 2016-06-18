4d169e47f9ea66bd639c70d6c114fb1d7d7fdebb
mrkiffie <mrkiffie@gmail.com>
2016-06-18 20:44:16 +0200

    Refactor fingerprinting to modify content and js

--- scripts/plugins/fingerprint-meta.js
+++ scripts/plugins/fingerprint-meta.js
@@ -1,12 +1,24 @@
 module.exports = function fingerprintmeta() {
   'use strict';
   return function (files, metalsmith, done) {
+    setImmediate(done);
     let map = metalsmith.metadata().fingerprint;
-    for (var file in files) {
-      if (files[file].styles) {
-        files[file].styles = files[file].styles.map(style => map[style]);
+    Object.keys(files).forEach(file => {
+      if (/\.(md|js)$/.test(file)) {
+        const data = files[file];
+        if (data.styles) {
+          data.styles = data.styles.map(style => map[style]);
+        }
+        if (data.scripts) {
+          data.scripts = data.scripts.map(script => map[script]);
+        }
+        let contents = data.contents.toString();
+        Object.keys(map).forEach(find => {
+          const replace = map[find];
+          contents = contents.split(find).join(replace);
+        });
+        data.contents = new Buffer(contents);
       }
-    }
-    done();
+    });
   };
 };
