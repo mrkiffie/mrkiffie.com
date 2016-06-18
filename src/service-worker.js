832cda0b16c2b164b03f3c7c72f73f98ed8fb1c0
mrkiffie <mrkiffie@gmail.com>
2016-06-18 22:58:54 +0200

    Add custom offline.svg

--- src/service-worker.js
+++ src/service-worker.js
@@ -19,6 +19,7 @@ function updateStaticCache() {
         '/assets/css/prism.css',
         '/assets/css/fancy-quotes.css',
         '/assets/img/kiffie.jpg',
+        '/assets/img/offline.svg',
         '/assets/js/main.js',
         '/assets/js/offline.js',
         '/',
@@ -137,10 +138,9 @@ self.addEventListener('fetch', event => {
             // OFFLINE
             // If the request is for an image, show an offline placeholder
             if (request.headers.get('Accept').indexOf('image') !== -1) {
-              return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {headers: {'Content-Type': 'image/svg+xml'}});
+              return caches.match('/assets/img/offline.svg');
             }
           });
       })
   );
-
 });
