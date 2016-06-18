c79da0f7e6cad517add2f44f8eda228c623efdc2
mrkiffie <mrkiffie@gmail.com>
2016-06-18 20:45:28 +0200

    Add offline page, template and js

---
+++ src/assets/js/offline.js
@@ -0,0 +1,17 @@
+if (window.caches) {
+  // Display posts that are available offline
+  caches.open('v0.01::pages')
+    .then(function(cache) {
+      cache.keys()
+        .then(function(keys) {
+          var cachedUrls = keys.map(function(req) {
+            return req.url;
+          });
+          [].slice.call(document.querySelectorAll('main a')).map(function(a) {
+            if (cachedUrls.indexOf(a.href) > -1) {
+              a.parentNode.hidden = false;
+            }
+          });
+        });
+    });
+}
---
+++ src/content/offline.md
@@ -0,0 +1,10 @@
+---
+layout: offline.hbs
+robots: "noindex, follow"
+title: Offline
+listing: true
+scripts:
+    - offline.js
+---
+
+You are offline and don't have access to new content. You do however have access to the following previously visited pages
