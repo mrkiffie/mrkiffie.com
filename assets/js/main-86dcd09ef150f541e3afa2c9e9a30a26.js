3951c8ed49d5253a7ce54ab8be96fab84ea43caa
mrkiffie <mrkiffie@gmail.com>
2016-08-12 08:56:55 +0200

    publish: Improve deployment process
    generated from commit 754bd1c293b3e853d4b986fbc3b48c18962aee8b

--- assets/js/main-86dcd09ef150f541e3afa2c9e9a30a26.js
+++
@@ -1,1 +0,0 @@
-'use strict';if(navigator.serviceWorker){navigator.serviceWorker.register('/service-worker.js',{scope:'/'});window.addEventListener('load',function(){if(navigator.serviceWorker.controller){navigator.serviceWorker.controller.postMessage({'command':'trimCaches'})}})}
--- assets/js/offline-f6f2722e3ec104f9318b50bb0d66c1db.js
+++
@@ -1,1 +0,0 @@
-'use strict';if(window.caches){caches.open('v1.0.1::pages').then(function(cache){cache.keys().then(function(keys){var cachedUrls=keys.map(function(req){return req.url});[].slice.call(document.querySelectorAll('main a')).map(function(a){if(cachedUrls.indexOf(a.href)>-1){a.parentNode.hidden=false}})})})}
