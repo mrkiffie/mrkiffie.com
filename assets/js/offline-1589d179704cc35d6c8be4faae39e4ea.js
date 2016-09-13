26239994350eeb868bbb1c957df96d0e97d6d5ce
mrkiffie <mrkiffie@gmail.com>
2016-09-13 18:25:25 +0200

    publish: Add fingerprinting to include json files - i.e. manifest.json
    generated from commit 4b8bbe3eaed04104890caa29e30f6d8576e5acb9

---
+++ assets/js/offline-1589d179704cc35d6c8be4faae39e4ea.js
@@ -0,0 +1,1 @@
+'use strict';if(window.caches){caches.open('v1.0.1::pages').then(function(cache){cache.keys().then(function(keys){var cachedUrls=keys.map(function(req){return req.url});[].slice.call(document.querySelectorAll('main a')).forEach(function(a){if(cachedUrls.includes(a.href)){a.parentNode.hidden=false}})})})}
